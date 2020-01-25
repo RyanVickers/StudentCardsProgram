eecb127b3bacc6558aa78db0a54cb2eaeb1ad175
RyanVickers <55283378+RyanVickers@users.noreply.github.com>
2020-01-25 08:46:23 -0500

    Lab 2 changes -added constructor -added get set methods with validation -added toString() method -add

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        Student ryan = new Student("ryan","vickers",11111810);
+        System.out.println(ryan.toString());
+    }
+}
