92757bcce597792c47bedef7e5b0f00f892e6fb4
RyanVickers <55283378+RyanVickers@users.noreply.github.com>
2020-01-25 08:43:09 -0500

    Lab 2 changes -added constructor -added get set methods with validation -added toString() method

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,81 @@
 public class Student {
-    private String firstName,lastName;
+    //Instance variables
+    private String firstName, lastName;
     private int studentNumber;
+
+    /**
+     * This constructor accepts a first name,last name and student #
+     * to create a student object with valid values
+     *
+     * @param firstName     must contain more than 1 letter
+     * @param lastName      must contain more than 1 letter
+     * @param studentNumber must be between 10000000-99999999
+     */
+    public Student(String firstName, String lastName, int studentNumber) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setStudentNumber(studentNumber);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * This method validates first name contains more than 1 letter
+     * and sets the first letter uppercase
+     *
+     * @param firstName must contain more than 1 letter
+     */
+    public void setFirstName(String firstName) {
+        if (firstName.length() > 1) {
+            firstName = firstName.substring(0, 1).toUpperCase() + firstName.substring(1);
+            this.firstName = firstName;
+        } else
+            throw new IllegalArgumentException("First name must contain more than 1 letter");
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * This method validates last name contains more than 1 letter
+     * and sets the first letter uppercase
+     *
+     * @param lastName must contain more than 1 letter
+     */
+    public void setLastName(String lastName) {
+        if (lastName.length() > 1) {
+            lastName = lastName.substring(0, 1).toUpperCase() + lastName.substring(1);
+            this.lastName = lastName;
+        } else
+            throw new IllegalArgumentException("Last name must contain more than 1 letter");
+
+    }
+
+    public int getStudentNumber() {
+        return studentNumber;
+    }
+
+    /**
+     * This method validates the student number is in the proper range
+     *
+     * @param studentNumber must be between 10000000-99999999
+     */
+    public void setStudentNumber(int studentNumber) {
+        if (studentNumber >= 10000000 && studentNumber <= 99999999)
+            this.studentNumber = studentNumber;
+        else
+            throw new IllegalArgumentException("The student number should be in the range 10000000-9999999");
+    }
+
+    /**
+     * This method formats the object into a string that can be output to the console
+     *
+     * @return
+     */
+    public String toString() {
+        return String.format("%s %s student #: %d%n", firstName, lastName, studentNumber);
+    }
 }
