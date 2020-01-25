546732299987a7ccdaaeb173f1e8e99dbc253a67
RyanVickers <55283378+RyanVickers@users.noreply.github.com>
2020-01-25 09:04:59 -0500

    Lab 2 changes -added constructor -added get set methods with validation -added toString() method -added Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        Student ryan = new Student("ryan","vickers",11111810);
+        Student ryan = new Student("Ryan","vickers",11111810);
         System.out.println(ryan.toString());
     }
 }
