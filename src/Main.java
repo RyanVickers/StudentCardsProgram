dbcbdef1f8d767db9df7962c41faa6c93c4a12b2
RyanVickers <55283378+RyanVickers@users.noreply.github.com>
2020-01-27 20:10:54 -0500

    Added GUI

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
+        primaryStage.setTitle("Student Card");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
     public static void main(String[] args) {
+        launch(args);
         Student ryan = new Student("Ryan","vickers",11111810);
         System.out.println(ryan.toString());
     }
